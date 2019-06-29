30d06968d74942f18a294bddb4b4650905009233
ernestques <ernesqr@yahoo.com>
2019-06-29 15:23:52 -0400

    table

--- tableinterview/src/app/table/table.component.ts
+++ tableinterview/src/app/table/table.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { sample_data } from '../../../mock/sample_data'
+import { sampledata } from '../../../mock/sample_data';
+import { DataSample } from '../models/model-data';
 
 @Component({
-		selector: 'app-table',
-	templateUrl: './table.component.html',
-	styleUrls: ['./table.component.scss']
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
 
-data = sample_data;
+  data: DataSample[] = sampledata;
+  headers: string[];
+  keys: string[];
 
-constructor() { }
 
-	ngOnInit() {
+  constructor() { }
+
+  ngOnInit() {
     console.log(this.data)
+    this.headersFromKeys(this.data);
+  }
+  headersFromKeys(data) {
+    // assuming the array is uniform
+    const ele = data.splice(1, 1);
+    this.headers = Object.keys(ele[0]);
 
-	}
+  }
 
 }
